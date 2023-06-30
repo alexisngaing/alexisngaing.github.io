a854378236fed6d949f6ee338504719931ac04ee
KiraraSan <ksankirara92@gmail.com>
2023-06-30 23:48:12 +0700

    fix about

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,25 +2,43 @@ import React from "react";
 
 function AboutMe() {
   return (
-    <div className="flex min-h-screen w-full bg-base-200">
-      <div className="flex-col lg:flex-row">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-base-200">
+      <div className="w-10/12">
         <div>
           <div className="flex items-center gap-12">
-            {/* <div className="w-[500px] h-[1px] bg-white"></div> */}
-            <h1 className="text-[64px]">About me</h1>
+            <div className="h-[2px] w-full bg-white"></div>
+            <h1 className="min-w-fit text-6xl">About me</h1>
           </div>
-          <p>
-            My name is Alexis Divasonda Sigat Ngaing, a programmer enthusiast. Here’s a glimpse of
-            me.
-          </p>
-          <br />
-          <p>
-            I was born and raised in East Borneo, Indonesia. I developed a keen interest in coding,
-            design, photography, and writing, which has shaped my perspective on live and influenced
-            my personal and professional endeavors.
-          </p>
-          <br />
-          <p>I studied Computer Science at Universitas Atma Jaya Yogyakarta.</p>
+          <div className="grid grid-cols-2 gap-12 p-10 text-2xl">
+            <div>
+              <p>
+                My name is Alexis Divasonda Sigat Ngaing, a programmer enthusiast. Here’s a glimpse
+                of me.
+              </p>
+              <br />
+              <p>
+                I was born and raised in East Borneo, Indonesia. I developed a keen interest in
+                coding, design, photography, and writing, which has shaped my perspective on live
+                and influenced my personal and professional endeavors.
+              </p>
+              <br />
+              <p>I studied Computer Science at Universitas Atma Jaya Yogyakarta.</p>
+            </div>
+            <div>
+              <p>
+                My name is Alexis Divasonda Sigat Ngaing, a programmer enthusiast. Here’s a glimpse
+                of me.
+              </p>
+              <br />
+              <p>
+                I was born and raised in East Borneo, Indonesia. I developed a keen interest in
+                coding, design, photography, and writing, which has shaped my perspective on live
+                and influenced my personal and professional endeavors.
+              </p>
+              <br />
+              <p>I studied Computer Science at Universitas Atma Jaya Yogyakarta.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
