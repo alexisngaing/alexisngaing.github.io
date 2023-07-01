7868d912b502c0483102516b236926d692cc8c75
KiraraSan <ksankirara92@gmail.com>
2023-07-01 12:06:50 +0700

    fix color & add one more grid

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -3,20 +3,20 @@ import React from "react";
 function Gallery() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-base-200">
-      <div className="w-10/12">
+      <div className="w-10/12 text-[#C0CAF5]">
         <div className="flex items-center gap-12">
-          <div className="h-[2px] w-full bg-white"></div>
           <h1 className="min-w-fit text-6xl font-bold">My works</h1>
+          <div className="h-[2px] w-full bg-[#C0CAF5]"></div>
         </div>
 
-        <div className="tabs tabs-boxed gap-6 py-6">
-          <a className="tab tab-active">Web dev</a>
-          <a className="tab">Design</a>
-          <a className="tab">Photo</a>
+        <div className="tabs tabs-boxed gap-6 py-6 text-[#C0CAF5]">
+          <a className="tab bg-[#414868] text-opacity-100 hover:bg-[#4b516b]">Web dev</a>
+          <a className="tab bg-[#414868] text-opacity-100 hover:bg-[#4b516b]">Design</a>
+          <a className="tab bg-[#414868] text-opacity-100 hover:bg-[#4b516b]">Photo</a>
         </div>
 
-        <div className="grid grid-cols-3 items-center justify-center">
-          <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="grid grid-cols-4 items-center justify-center px-4">
+          <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
             <figure className="px-5 pt-5">
               <img alt="Shoes" className="rounded-sm" src="/images/cardtest.jpg" />
             </figure>
@@ -25,7 +25,7 @@ function Gallery() {
               <p>Main page</p>
             </div>
           </div>
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
             <figure className="px-5 pt-5">
               <img alt="Shoes" className="rounded-sm" src="/images/cardtest.jpg" />
             </figure>
@@ -34,7 +34,16 @@ function Gallery() {
               <p>Main page</p>
             </div>
           </div>
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
+            <figure className="px-5 pt-5">
+              <img alt="Shoes" className="rounded-sm" src="/images/cardtest.jpg" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Web KSL-UAJY</h2>
+              <p>Main page</p>
+            </div>
+          </div>
+          <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
             <figure className="px-5 pt-5">
               <img alt="Shoes" className="rounded-sm" src="/images/cardtest.jpg" />
             </figure>
