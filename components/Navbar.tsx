63d0576c7d633c12da334f527c890305c3e08be5
KiraraSan <ksankirara92@gmail.com>
2023-07-01 12:07:15 +0700

    position fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed z-20 bg-base-100 text-[#C0CAF5]">
       <div className="flex-1 px-6">
         <a className="btn-ghost btn text-xl normal-case">alex</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-6">
           <li>
-            <a>Home</a>
+            <a href="/">Home</a>
           </li>
           <li>
             <a>About</a>
