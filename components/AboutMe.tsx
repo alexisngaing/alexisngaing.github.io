f668e1f6d98bfa0795c1b82264ad87864bda54df
KiraraSan <ksankirara92@gmail.com>
2023-07-02 22:10:15 +0700

    add more information

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -21,21 +21,26 @@ function AboutMe() {
               influenced my personal and professional endeavors.
             </p>
             <br />
-            <p>I studied Computer Science at Universitas Atma Jaya Yogyakarta.</p>
+            <p>
+              I studied Computer Science at Universitas Atma Jaya Yogyakarta. At my university, I
+              served as the chairman of the Linux Study Group for the 2022/2023 period. I also
+              joined the board of the Computer Science Student Association (HIMAFORKA) as a member
+              of the Academic and Achievement (APRES) committee.
+            </p>
           </div>
           <div>
             <p>
-              My name is Alexis Divasonda Sigat Ngaing, a programmer enthusiast. Here’s a glimpse of
-              me.
+              Some of the subjects I studied in my courses include Basic Programming, which uses the
+              C language, Data Structures (C), Databases (SQL), Object-Oriented Programming (Java),
+              and Machine Learning & Deep Learning (Python).
             </p>
             <br />
             <p>
-              I was born and raised in East Borneo, Indonesia. I developed a keen interest in
-              coding, design, photography, and writing, which has shaped my perspective on live and
-              influenced my personal and professional endeavors.
+              My skills include: beginner in C, Java, Python, SQL, and Linux. I am also a beginner
+              in HTML, CSS, JavaScript, and TypeScript, which I have learned self-taught.
             </p>
             <br />
-            <p>I studied Computer Science at Universitas Atma Jaya Yogyakarta.</p>
+            <p>Intermediate at Photography and Design (specially Photoshop and Figma).</p>
           </div>
         </div>
       </div>
