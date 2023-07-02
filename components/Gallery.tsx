5937f0f2d2d848291f43f2d5a1af3bdea3147627
KiraraSan <ksankirara92@gmail.com>
2023-07-02 18:42:50 +0700

    fix card

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -9,14 +9,25 @@ function Gallery() {
           <div className="h-[2px] w-full bg-[#C0CAF5]"></div>
         </div>
 
-        <div className="tabs tabs-boxed gap-6 py-6 text-[#C0CAF5]">
+        <div className="tabs tabs-boxed gap-6 pt-10 text-[#C0CAF5]">
           <a className="tab bg-[#414868] text-opacity-100 hover:bg-[#4b516b]">Web dev</a>
           <a className="tab bg-[#414868] text-opacity-100 hover:bg-[#4b516b]">Design</a>
           <a className="tab bg-[#414868] text-opacity-100 hover:bg-[#4b516b]">Photo</a>
         </div>
 
-        <div className="grid grid-cols-4 items-center justify-center px-4">
-          <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
+        <div className="grid grid-cols-3 pt-6">
+          <div className="card w-96 rounded-sm bg-base-100 shadow-xl hover:bg-white">
+            <a href="http://">
+              <figure className="px-5 pt-5">
+                <img alt="ksl" className="rounded-sm" src="/images/ksl.png" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Web KSL-UAJY</h2>
+                <p>Main page</p>
+              </div>
+            </a>
+          </div>
+          {/* <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
             <figure className="px-5 pt-5">
               <img alt="Shoes" className="rounded-sm" src="/images/cardtest.jpg" />
             </figure>
@@ -33,8 +44,8 @@ function Gallery() {
               <h2 className="card-title">Web KSL-UAJY</h2>
               <p>Main page</p>
             </div>
-          </div>
-          <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
+          </div> */}
+          {/* <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
             <figure className="px-5 pt-5">
               <img alt="Shoes" className="rounded-sm" src="/images/cardtest.jpg" />
             </figure>
@@ -42,16 +53,7 @@ function Gallery() {
               <h2 className="card-title">Web KSL-UAJY</h2>
               <p>Main page</p>
             </div>
-          </div>
-          <div className="card w-96 rounded-sm bg-base-100 shadow-xl">
-            <figure className="px-5 pt-5">
-              <img alt="Shoes" className="rounded-sm" src="/images/cardtest.jpg" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Web KSL-UAJY</h2>
-              <p>Main page</p>
-            </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
